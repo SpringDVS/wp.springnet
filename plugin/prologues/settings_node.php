991c9dd830673473b2d9b69c142dabde4191eecb
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-09-21 13:36:47 +0100

    Add clean-up and enhancement to settings overview

--- plugin/prologues/settings_node.php
+++ plugin/prologues/settings_node.php
@@ -1,8 +1,17 @@
 <?php
 require SPRINGNET_DIR.'/plugin/models/class-node-model.php';
+require_once SPRINGNET_DIR.'/plugin/models/class-keyring-model.php';
+
 $node = new Node_Model();
-$is_registered =  $node->is_registered();
+$keyring = new Keyring_Model();
+
+$has_public_cert = $keyring->has_certificate();
+$has_uri = get_option('node_uri') != '' ? true : false;
+$has_token = get_option('geonet_token') != '' ? true : false;
+$is_registered = $node->is_registered();
 $is_enabled = false;
+
+
 if($is_registered) {
 	$is_enabled = $node->is_enabled();
 }
--- plugin/views/settings_node.php
+++ plugin/views/settings_node.php
@@ -1,6 +1,18 @@
 <?php defined( 'ABSPATH' ) or die( 'Error' ); ?>
 <div class="notice notice-error" id="error-banner" style="display:none;"></div>
 
+<?php if(!$has_public_cert):?>
+	<div class="notice notice-error" id="error-banner">
+		Node does not have a public certificate -- please use the 
+		<em>Certificate</em> tab to generate one.
+	</div>
+<?php endif; ?>
+<?php if(!$has_uri || !$has_token):?>
+	<div class="notice notice-error" id="error-banner">
+		Node does not have a valid network setup -- please use the 
+		<em>Network</em> tab to configure.
+	</div>
+<?php endif; ?>
 <table class="form-table">
   <tr>
     <th>Registration Status</th>
@@ -16,7 +28,10 @@
   </tr>
 </table>
 
-<?php if(!$is_registered): ?>
+<?php if(!$has_public_cert || !$has_uri || !$has_token): ?>
+<button disabled>Bring Online</button>
+<button disabled>Register Node</button>
+<?php elseif(!$is_registered): ?>
 <button disabled>Bring Online</button>
 <button id="node-register-button">Register Node</button>
 <?php elseif (!$is_enabled): ?>
@@ -25,5 +40,38 @@
 <?php elseif ($is_enabled): ?>
 <button id="node-state-disable">Bring Offline</button>
 <button disabled>Register Node</button>
+<?php endif; ?>
+
+<?php if($has_uri): ?>
+
+<table class="form-table">
+  <tr>
+    <th>URI</th>
+    <td id="info-node-status">
+    <?php echo esc_attr(get_option('node_uri')); ?>
+    </td>
+  </tr>
+  
+  <tr>
+    <th>GeoNetwork</th>
+    <td id="info-node-registered">
+    <?php echo esc_attr(get_option('geonet_name')); ?>.uk
+    </td>
+  </tr>
+    
+  <tr>
+    <th>Primary Service</th>
+    <td id="info-node-status">
+    <?php echo esc_attr(get_option('geonet_hostname')); ?>
+    </td>
+  </tr>
+
+  <tr>
+    <th>Node Service</th>
+    <td id="info-node-status">
+    <?php echo esc_attr(get_option('node_hostname')); ?>
+    </td>
+  </tr>
+</table>
 <?php endif; ?>
 
