757e847a70dcdee9d1e4ec2946118d15c9a62770
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-09-24 17:24:54 +0100

    Add start of Overview page

--- plugin/views/plugin_overview.php
+++ plugin/views/plugin_overview.php
@@ -1,4 +1,53 @@
 <div class="wrap">
-<?php springnet_uri_tag(); ?>
-<h1>Overview</h1>
+	<?php springnet_uri_tag(); ?>
+	<h1>Overview</h1>
+	
+	<div class="metabox-holder">
+		<div class="postbox-container">
+			<div class="meta-box-sortables ui-sortable">
+
+
+				<div class="postbox">
+				
+					<button type="button" class="handlediv button-link" aria-expanded="true">
+						<span class="screen-reader-text">Toggle panel: Notification</span>
+						<span class="toggle-indicator" aria-hidden="true"></span>
+					</button>		
+					<h2 class="hndle ui-sortable-handle">
+						<span>Notifications</span>
+					</h2>
+					
+					<div class="inside">
+						<div class="activity-block">Foobar</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		
+
+		<div class="postbox-container">
+			<div class="meta-box-sortables ui-sortable">
+				<div class="postbox">
+				
+					<button type="button" class="handlediv button-link" aria-expanded="true">
+						<span class="screen-reader-text">Toggle panel: Notification</span>
+						<span class="toggle-indicator" aria-hidden="true"></span>
+					</button>		
+					<h2 class="hndle ui-sortable-handle">
+						<span>spring:// Network News</span>
+					</h2>
+					
+					<div class="inside">
+						<div class="activity-block">Foobar</div>
+					</div>
+				</div>
+
+		
+			</div>
+		</div>	
+	
+	</div>
+
 </div>
+
