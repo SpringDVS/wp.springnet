6979983da0c37bed5ef9ba1060e964f3bb268c8a
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-09-28 17:09:30 +0100

    Change license for plugin
    The license is now GPLv3 because the netlib is licensed as Apache v2

--- springnet.php
+++ springnet.php
@@ -5,15 +5,16 @@ ini_set('display_errors', 1);
 /*
 Plugin Name: SpringNet
 Plugin URI:  http://spring-dvs.org
-Description: Plugin for running a Spring Network node
+Description: Plugin for running a Spring Network node off the back of Wordpress
 Version:     0.1.0
 Author:      The Care Connections Initiative c.i.c
 Author URI:  http://spring-dvs.org
-License:     GPL2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
+License:     GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
 Domain Path: /languages
 Text Domain: springnet
 */
+
 
 defined( 'ABSPATH' ) or die( 'Error' );
 
