2d9d501fc89df97b8c6603c568229ad76344cf4c
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-10-06 11:18:07 +0100

    updater: Fix bug that overwrites other plugin information
    The function called on the plugins_api hook was overwriting other 
    Wordpress information received for the 'Add Plugins' page or details
    about other plugins when requested. This fix corrects the behaviour of
    the plugin in the update portion.

--- plugin/admin/update.php
+++ plugin/admin/update.php
@@ -2,17 +2,22 @@
 add_filter ('pre_set_site_transient_update_plugins', 'springnet_update_plugin_check');
 add_filter('plugins_api', 'springnet_plugin_update_info', 10, 3);
 
-function springnet_update_plugin_check ($aggregate)
+function springnet_update_plugin_check ($transient)
 {
+	if ( empty( $transient->checked ) ) {
+		return $transient;
+	}
 	$response = wp_remote_get("http://spring-dvs.org/versions/wp.springnet.json");
-	$json = $response['body'];
+
+	$json = wp_remote_retrieve_body($response);
+
 	$info = json_decode($json, true);
 
 	$data = get_plugin_data(SPRINGNET_DIR.'/springnet.php', false, false);
 	$local = $data['Version'];
 
 	if(version_compare($local, $info['version']) >= 0) {
-		return $aggregate;
+		return $transient;
 	}
 	$obj = new stdClass();
 	$obj->slug = 'springnet';
@@ -22,24 +27,29 @@ function springnet_update_plugin_check ($aggregate)
 	$obj->tested = $info['tested'];
 	$obj->package = 'http://packages.spring-dvs.org/wp.springnet_'.$info['version'].".zip";
 	$obj->sections = $info['sections'];
-	$aggregate->response['springnet/springnet.php'] = $obj;
+	$transient->response['springnet/springnet.php'] = $obj;
 
-	return $aggregate;
+	return $transient;
 }
 
-function springnet_plugin_update_info() {
-	$response = wp_remote_get("http://spring-dvs.org/versions/wp.springnet.json");
-	$json = $response['body'];
+function springnet_plugin_update_info($false, $action, $response) {
+
+	if ( empty( $response->slug ) || $response->slug != 'springnet' ) {
+    	return false;
+	}
+
+	$details = wp_remote_get("http://spring-dvs.org/versions/wp.springnet.json");
+	$json = wp_remote_retrieve_body($details);
 	$info = json_decode($json, true);
 
-	$obj = new stdClass();
-	$obj->slug = 'springnet';
-	$obj->plugin = 'springnet';
-	$obj->new_version = $info['version'];
-	$obj->url = 'http://www.spring-dvs.org';
-	$obj->tested = $info['tested'];
-	$obj->package = 'http://packages.spring-dvs.org/wp.springnet_'.$info['version'].".zip";
-	$obj->sections = $info['sections'];
-	$obj->name = 'SpringNet Node Plugin';
-	return $obj;
+	
+	$response->slug = 'springnet';
+	$response->plugin = 'springnet';
+	$response->new_version = $info['version'];
+	$response->url = 'http://www.spring-dvs.org';
+	$response->tested = $info['tested'];
+	$response->package = 'http://packages.spring-dvs.org/wp.springnet_'.$info['version'].".zip";
+	$response->sections = $info['sections'];
+	$response->name = 'SpringNet Node Plugin';
+	return $response;
 }
